9ae681a049ec3f128578bbfa676691ae9b94f904
fabian <180667352+fabo0822@users.noreply.github.com.>
2025-10-24 22:34:11 -0500

    style:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logoImagen from "../../assets/globant.png";
-
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src={logoImagen} alt="Logo" />
+      <img src={logoImagen} alt="Logo" className="logo" />
       <ul className="tabs">
         <li>Overview</li>
         <li>Contacts</li>
