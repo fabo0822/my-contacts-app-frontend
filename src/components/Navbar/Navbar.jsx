87c2c13250d851416fa3e63c2c3823c2472aabe5
fabian <180667352+fabo0822@users.noreply.github.com.>
2025-10-25 10:25:37 -0500

    feat(navbar):add active tab funtionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import logoImagen from "../../assets/globant.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  // state to store wich tab is active
+  const [activeTab, setActiveTab] = useState("Overview");
+
+
+  //funtion to change the active tab when user clicks
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  }
   return (
     <nav className="navbar">
       <img src={logoImagen} alt="Logo" className="logo" />
       <ul className="tabs">
-        <li>Overview</li>
-        <li>Contacts</li>
-        <li>Favorites</li>
+        <li className={activeTab === "Overview" ? "active" : ""} onClick={() => handleTabClick("Overview")}> Overview</li>
+        <li className={activeTab === "Contacts" ? "active" : ""} onClick={() => handleTabClick("Contacts")}> Contacts</li>
+        <li className={activeTab === "Favorites" ? "active" : ""} onClick={() => handleTabClick("Favorites")}> Favorites</li>
       </ul>
 
       <button className="new-button">+ NEW</button>
